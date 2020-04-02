3ff323eda6831b0a102b812ae0fd6e1fab5496e2
LeonidTheGod <king.sneakers.che@gmail.com>
2020-04-03 00:14:10 +0300

    ver2, mover fd to if block

--- fractals/koh_fractal.py
+++ fractals/koh_fractal.py
@@ -4,37 +4,33 @@ import sys
 sys.setrecursionlimit(2500)
 turtle.speed(100)
 turtle.penup()
-turtle.hideturtle()
+# turtle.hideturtle()
 turtle.goto(-200, -300)
 turtle.left(90)
 turtle.pendown()
 
 def koh(n, size):
     if n == 0:
+        turtle.fd(size)
         return
 
     koh(n - 1, size)
     turtle.right(60)
-    turtle.fd(size)
 
     koh(n - 1, size)
     turtle.left(120)
-    turtle.fd(size)
 
     koh(n - 1, size)
     turtle.right(60)
-    turtle.fd(size)
 
     koh(n - 1, size)
 
-
-n = 7
+n = 2
 try:
-    size = 10/n
+    size = 50/n
 except:
     size = 5
 
-turtle.fd(size)
 koh(n, size=size)
 
 turtle.exitonclick()
